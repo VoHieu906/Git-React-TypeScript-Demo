a75b1b4f3b814f64d97e780d5c178480756d937d
VoHieu906 <vohieutd09@gmail.com>
2024-08-23 09:36:05 +0700

    thêm footerpage

--- git-react-typescript/src/App.jsx
+++ git-react-typescript/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HeaderPage from "./components/Layout/HeaderPage";
+import FooterPage from "./components/Layout/FooterPage";
 
 function App() {
   return (
     <>
       <Router>
         <HeaderPage />
+        <FooterPage />
       </Router>
     </>
   );
